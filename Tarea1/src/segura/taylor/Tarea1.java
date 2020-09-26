3a77b233f4da7b79c59a5821142a91662f2fbede
TaylorSeguraVindas <tsegurav@ucenfotec.ac.cr>
2020-09-26 15:14:20 -0600

    primer commit

---
+++ Tarea1/src/segura/taylor/Tarea1.java
@@ -0,0 +1,12 @@
+package segura.taylor;
+
+/**
+ * @author Taylor Segura Vindas
+ */
+public class Tarea1 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
